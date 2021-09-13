3cb026889443284ce1180097f095c3e3f76dde62
vadhelg <vadhelg@gmail.com>
2021-09-13 20:00:12 +0530

    TASK-2
    Mark todo as completed.

--- resources/fixtures.sql
+++ resources/fixtures.sql
@@ -3,12 +3,12 @@ INSERT INTO users (username, password) VALUES
 ('user2', 'user2'),
 ('user3', 'user3');
 
-INSERT INTO todos (user_id, description) VALUES
-(1, 'Vivamus tempus'),
-(1, 'lorem ac odio'),
-(1, 'Ut congue odio'),
-(1, 'Sodales finibus'),
-(1, 'Accumsan nunc vitae'),
-(2, 'Lorem ipsum'),
-(2, 'In lacinia est'),
-(2, 'Odio varius gravida');
+INSERT INTO todos (user_id, description, status) VALUES
+(1, 'Vivamus tempus','Pending'),
+(1, 'lorem ac odio','Pending'),
+(1, 'Ut congue odio','Pending'),
+(1, 'Sodales finibus','Pending'),
+(1, 'Accumsan nunc vitae','Pending'),
+(2, 'Lorem ipsum','Pending'),
+(2, 'In lacinia est','Pending'),
+(2, 'Odio varius gravida','Pending');
